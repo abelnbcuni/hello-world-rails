1f20fdac583298e039c619e32579c855f7c95e00
Abel Lawal <abellawal@yahoo.com>
2017-12-31 14:11:10 -0800

    Adding Env Functionality For Testing and Changing Routing File

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  get 'env_info/index'
+  #get 'env_info/index'
 
-  #get 'mysql/index'
-  #get 'mysql/edit'
-  #get 'home/index'
+ # get 'mysql/index'
+ # get 'mysql/edit'
+ # get 'home/index'
+ # get 'env_info/index'
 
   get '/', to: 'home#index'
   get '/mysql', to: 'mysql#index'
   get '/redis', to: 'redis#index'
   post '/mysql/new', to: 'mysql#save'
   get 'mysql/new', to: 'mysql#new'
-  get '/env', to: 'env_info#index'
+  get '/env', to: 'env_info#index', as: 'env'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
