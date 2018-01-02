425c3fb7eda971d7d5701799970a9e9703ca6276
Abel Lawal <abellawal@yahoo.com>
2018-01-01 18:21:09 -0800

    Work in progress commit

--- app/controllers/env_info_controller.rb
+++ app/controllers/env_info_controller.rb
@@ -1,7 +1,9 @@
+require 'json'
+
 class EnvInfoController < ApplicationController
   def index
-    @env_app = ENV["VCAP_APPLICATION"] ? ENV["VCAP_APPLICATION"].to_h : "No PCF Environment Application Info Found"
-    @env_services = ENV["VCAP_SERVICES"] ? ENV["VCAP_SERVICES"].to_h : "No PCF Environment Service Info Found"
+    @env_app = ENV["VCAP_APPLICATION"] ? JSON.pretty_generate(ENV["VCAP_APPLICATION"]) : "No PCF Environment Application Info Found"
+    @env_services = ENV["VCAP_SERVICES"] ? JSON.pretty_generate(ENV["VCAP_SERVICES"]) : "No PCF Environment Service Info Found"
     render 'index'
   end
 end
